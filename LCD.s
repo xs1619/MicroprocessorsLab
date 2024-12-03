bf20448e1474f81982e5668399d33db8131bcb71
xs1619 <xs1619@skbl-418-004.ph.ic.ac.uk>
2024-12-03 13:00:45 +0000

    3rd Dec

--- LCD.s
+++ LCD.s
@@ -1,6 +1,6 @@
 #include <xc.inc>
 
-global  LCD_Setup, LCD_Write_Message, LCD_line2,LCD_Send_Byte_D
+global  LCD_Setup, LCD_Write_Message, LCD_line2,LCD_Send_Byte_D, LCD_delay_ms
 
 psect	udata_acs   ; named variables in access ram
 LCD_cnt_l:	ds 1   ; reserve 1 byte for variable LCD_cnt_l
