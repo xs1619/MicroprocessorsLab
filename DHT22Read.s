dc51b28e217d41a248f0c1910435a84917fdd384
xs1619 <xs1619@skbl-418-004.ph.ic.ac.uk>
2024-12-06 16:05:08 +0000

    6 Dec

--- DHT22Read.s
+++ DHT22Read.s
@@ -1,13 +1,12 @@
 #include <xc.inc>
     
-global  DHT22_Setup, DHT22_Read, Read_Byte, delay_short, hum_data, temp_data, Read_Bit 
-extrn	LCD_delay_ms
+global  DHT22_Setup, DHT22_Read, Read_Byte, delay_short, data_buffer, Read_Bit 
+extrn	LCD_delay_ms, LCD_delay_x4us
 
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
-temp_data:  ds 2    ; reserve two bytes for temperature data
-hum_data:   ds 2    ; reserve two bytes for humidity data
+data_buffer:  ds 5    ; reserve five bytes for temperature data
     
 psect	DHT22_code,class=CODE
     ; ******* DHT22 Read Subroutine *********************************
@@ -20,6 +19,8 @@ DHT22_Setup:
 
 DHT22_Read:
     ; Initialize the sensor
+    movlw   90 
+    call    LCD_delay_ms
     bcf     TRISJ, 0, A       ; Set RH0 as output
     bcf     LATJ, 0, A       ; Pull data line low
     movlw   0xff           ; Wait at least 18ms
@@ -34,34 +35,40 @@ DHT22_Read:
 
     ; Wait for sensor response
     btfss   PORTJ, 0, A         ; Wait for the line to go low
-    bra     $-1
+    bra     $-2
     btfsc   PORTJ, 0, A      ; Wait for the line to go high
-    bra     $-1   
+    bra     $-2   
     btfss   PORTJ, 0, A      ; Wait for the line to go low again
-    bra     $-1
+    bra     $-2
 
     ; Read 40 bits of data (5 bytes)
     movlw   5                ; 5 bytes to read
     movwf   counter, A
-    clrf    temp_data , A       ; Clear temperature data
-    clrf    temp_data+1, A
-    clrf    hum_data, A         ; Clear humidity data
-    clrf    hum_data+1, A
+    clrf    data_buffer , A       ; Clear buffer data
+    ; loop to read 5 bytes
+    
 
 Read_Byte:
     movlw   8                ; 8 bits per byte
     movwf   delay_count, A
-    return
+    ; loop to read 8 bits
+    
 
 Read_Bit:
     btfss   PORTJ, 0, A         ; Wait for the line to go high
-    bra     $-1
-    call    delay_short    ; Wait for 30us
+    bra     $-2
+    movlw   8
+    call    LCD_delay_x4us    ; Wait for 32us
     btfsc   PORTJ, 0, A         ; If the line is still high, it's a 1
-    bsf     hum_data+1, 7, A    ; Store the bit
+    bsf     data_buffer+4, 0, A    ; Store the bit
     btfss   PORTJ, 0, A         ; Wait for the line to go low
-    bra     $-1
-    rlcf    hum_data+1, F, A    ; Rotate left through carry
+    bra     $-2
+    bcf	    CARRY
+    rlcf    data_buffer+4, F, A    ; Rotate left through carry
+    rlcf    data_buffer+3, F, A    ; Rotate left through carry
+    rlcf    data_buffer+2, F, A    ; Rotate left through carry
+    rlcf    data_buffer+1, F, A    ; Rotate left through carry
+    rlcf    data_buffer, F, A    ; Rotate left through carry
     decfsz  delay_count, A
     bra     Read_Bit
  
